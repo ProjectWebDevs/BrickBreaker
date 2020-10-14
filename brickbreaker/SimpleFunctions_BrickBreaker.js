c950a0373304359a70467c3e837b28443d801697
Nik Wurmel <nikolaswurmel@gmail.com>
2020-10-14 06:41:46 -0600

    Simple functions needed for game operation
    simple function templates describing what needs to be done

---
+++ brickbreaker/SimpleFunctions_BrickBreaker.js
@@ -0,0 +1,17 @@
+function moveBall(ball) {
+    //using ball.speed and ball.dir determine the next point the ball will be in
+    //there is porbably an existing fucntion in js which accomplishes this task
+    //convert one type of coords into another (speed, dir) into (dy,dy)
+
+}
+function moveBar(bar,x) {
+    //user input will move the bar quite obviously
+    //the bar should only be able to move at its set bar.speed attribute
+    //this function will move the bar one frame at a time to an appropiate position according to the ruses of the game
+}
+function exitGame() {
+    //quit button 
+    //quite simple
+    //returns the user to the main menu
+    //Could simply change the gameState variable
+}
